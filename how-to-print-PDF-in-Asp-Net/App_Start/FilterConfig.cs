af8342158cd516a7fcb024d449ce76bda9c1ec9c
Sreekanth S <sreeacl@yahoo.com>
2019-06-28 16:51:27 +0530

    Add project files.

---
+++ how-to-print-PDF-in-Asp-Net/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace how_to_print_PDF_in_Asp_Net
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ how-to-print-PDF-in-Asp-Net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="how_to_print_PDF_in_Asp_Net.MvcApplication" Language="C#" %>
