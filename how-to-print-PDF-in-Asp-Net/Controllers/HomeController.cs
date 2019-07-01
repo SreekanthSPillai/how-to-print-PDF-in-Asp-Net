ee6599590dde564417e40ff71a653d0bcc9234f6
Sreekanth S <sreeacl@yahoo.com>
2019-07-01 15:27:59 +0530

    PDF Conversion PoC - Rotativa, DinkToPdf

--- how-to-print-PDF-in-Asp-Net/Controllers/HomeController.cs
+++ how-to-print-PDF-in-Asp-Net/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
+using DinkToPdf;
+using how_to_print_PDF_in_Asp_Net.Models;
+using Rotativa;
+using Rotativa.Options;
 
 namespace how_to_print_PDF_in_Asp_Net.Controllers
 {
@@ -16,15 +22,43 @@ namespace how_to_print_PDF_in_Asp_Net.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
+            return new ViewAsPdf();
+            //return new ActionAsPdf("Index", new { name = "Giorgio" }) { FileName = "Test.pdf", ContentDisposition = ContentDisposition.Inline };
+            //return View();
+            //Install - Package Rotativa
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+            DinkToPdf.Contracts.IConverter _converter = new SynchronizedConverter(new PdfTools());
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = DinkToPdf.Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "Owned",
+                Out = @"C:\Users\Jodson\test.pdf"
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            var file = _converter.Convert(pdf);
+            return File(file, "application/pdf");
         }
     }
 }
---
+++ how-to-print-PDF-in-Asp-Net/Models/TemplateGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace how_to_print_PDF_in_Asp_Net.Models
+{
+    public static class TemplateGenerator
+    {
+        public static string GetHTMLString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(@"<html>
+                <head>
+                </head>
+                <body>
+                  <div id=teste><div>
+                </body>");
+            sb.Append(@"<script>
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {                                         document.getElementById('teste')
+        .innerHTML=this.response;
+                };
+                xhttp.open('GET', 'file:\\\C:/somefile.txt, true);
+                xhttp.send();
+              </script>
+            </html>");
+            return sb.ToString();
+        }
+    }
+}
